581773bc9d97a48c4161fdf8b0b9a60fa115da37
Junlin <junlinzhou@yzbigdata.com>
2020-07-13 10:51:45 +0800

    Initial config-client properties

---
+++ src/main/java/org/zjl/spring/cloud/tuto/ConfigServer.java
@@ -0,0 +1,16 @@
+package org.zjl.spring.cloud.tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Junlin Zhou
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigServer.class, arguments);
+    }
+}
